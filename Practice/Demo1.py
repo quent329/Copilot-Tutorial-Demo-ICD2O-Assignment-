701e373431ce9f0c29dcc373d65cc421384809b8
Quentin Lam <quentin.lam2@student.tdsb.on.ca>
2024-11-24 19:21:31 -0500

    Update Demo1.py

--- Practice/Demo1.py
+++ Practice/Demo1.py
@@ -1,1 +1,3 @@
-
+'''
+Instructions: Write a function with two parameters, area and height, for a rectangle. The function must return/print out the area of the rectangle once the two parameter values have been given.
+'''
